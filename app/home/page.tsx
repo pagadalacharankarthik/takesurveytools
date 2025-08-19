10476a944275239e41249c54c63cf0c028866c53
Pagadala Charan karthik <charankarthik366@gmail.com>
2025-08-19 22:53:57 +0530

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -78,7 +78,17 @@ export default function HomePage() {
                 <ArrowRight className="ml-2 h-4 w-4" />
               </Button>
             </Link>
-            <Button size="lg" variant="outline" className="w-full sm:w-auto bg-transparent"> View Demo </Button>
+            <a
+            href="https://drive.google.com/file/d/1L_UiJOJenrOyShaGyMKUBiC1krw9dgUJ/view?usp=drivesdk"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="w-full sm:w-auto">
+              <Button
+                size="lg"
+                variant="outline"
+                className="w-full sm:w-auto bg-transparent">View Demo</Button>
+            </a>
+
           </div>
         </div>
       </section>
